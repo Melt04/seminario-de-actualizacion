af5d2b4cf3628d358911987d4c50dd563c9d2a28
Alan Garcia <alanmdq14@gmail.com>
2023-05-30 09:14:27 -0300

    se modifica la estructura de los archivos

---
+++ model/entities/users.js
@@ -0,0 +1,9 @@
+class User {
+  constructor (name, lastName, groupId, status) {
+    this.name = name
+    this.lastName = lastName
+    this.groupId = groupId
+    this.status = status
+  }
+}
+module.exports = User
